899a9694da624994eb2464219812357ee2a874d2
Umesh R Bilagi <umeshbilagi@gmail.com>
2018-01-14 06:40:40 +0530

    JWT

--- js-dist/jwt.js
+++ js-dist/jwt.js
@@ -9,3 +9,17 @@ function decode_base64(encStr) {
     let result = wordArray.toString(curCrypt.enc.Utf8);
     return result;
 }
+function encrypt(strBase64, secret) {
+    return curCrypt.HmacSHA256(strBase64, secret);
+}
+function createJWT(payload, secret) {
+    let header = {
+        "typ": "JWT",
+        "alg": "HS256"
+    };
+    let base64Header = encode_base64(header);
+    let base64Payload = encode_base64(payload);
+    let signeture = encrypt(base64Header + "." + base64Payload, secret);
+    let base64Signeture = curCrypt.enc.Base64.stringify(signeture);
+    return base64Header.base64Payload.base64Signeture;
+}
